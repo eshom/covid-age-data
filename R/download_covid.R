ec766b4643ba491bb0335e63ae13a8e2c873ad34
eshom <erez@mandor>
2020-12-30 19:46:16 +0200

    Updated functions and documentation
    Switched from using 'readr' to 'data.table' as a backend for reading the files.
    It's faster, which great for reading millions of rows, and it also avoids a
    weird issue I had with 'readr::read_csv()' causing a segfault.

--- R/download_covid.R
+++ R/download_covid.R
@@ -2,20 +2,20 @@
 #' It reads the downloaded data and converts it into a data frame.
 #'
 #' This function uses the OSF API to download the publicly available COVerAGE-DB
-#' datasets. It then returns the downloaded dataset as a data frame or tibble.
-#' There are 4 possible datasets available for download: "inputDB", "output_5",
-#' "output_10", "qualityMetrics".
+#' datasets. It then returns the downloaded dataset as a data frame, data table, or tibble.
+#' There are 4 possible datasets available for download: "inputDB", "Output_5",
+#' "Output_10", "qualityMetrics".
 #' See <https://timriffe.github.io/covid_age/GettingStarted.html> for more
-#' information about these datasets and the reading routine
-#' used in this function.
+#' information about these datasets.
 #' @seealso [osfr::osf_retrieve()] for the OSF entity retrieval function;
-#' [osfr::osf_download()] for the downloading function; [readr::read_csv()] for
+#' [osfr::osf_download()] for the downloading function; [data.table::fread()] for
 #' the reading function.
 #' @title Download COVerAGE-DB data
 #' @param data The name of the dataset to download. Can be one of the
-#' the following: "inputDB", "output_5", "output_10", "qualityMetrics".
+#' the following: "inputDB", "Output_5", "Output_10", "qualityMetrics".
 #' @param temp Logical. Should the dataset be downloaded to a temporary directory?
-#' @param tibble Logical. should the function return a tibble?
+#' @param return What should be the return type? Can be on of the following:
+#' "data.frame", "data.table", "tibble".
 #' @param progress Passed to [osfr::osf_download()]. Logical, if TRUE
 #' progress bars are displayed for each file transfer. Mainly useful for
 #' transferring large files. For tracking lots of small files, setting
@@ -36,52 +36,98 @@
 #' will determine the number of levels to recurse.
 #' @param verbose Passed to [osfr::osf_download()] Logical, indicating whether
 #' to print informative messages about interactions with the OSF API (default FALSE).
-#' @param ... Additional named arguments passed to [readr::read_csv()]
-#' @return By default a data frame or a tibble. The return value is controlled
-#' by the "tibble" parameter.
+#' @param ... Additional named arguments passed to [data.table::fread()]
+#' @return By default a data frame with the uncompressed dataset.
+#' Can be set to return either a data table or
+#' a tibble. The return type is controlled by the "return" parameter.
 #' @author Erez Shomron
 #' @examples
 #' \dontrun{
 #' # Basic usage
 #' df <- download_covid("inputDB")
 #' # Download data to the working directory.
-#' df <- download_covid("output_5", temp = FALSE)
+#' df <- download_covid("Output_5", temp = FALSE)
 #' # If you want a tibble instead of a data frame:
-#' df <- download_covid("output_10", tibble = TRUE)
+#' df <- download_covid("Output_10", return = "tibble")
 #' }
 #'
 #' @export
-download_covid <- function(data = c("inputDB", "output_5", "output_10",
+download_covid <- function(data = c("inputDB", "Output_5", "Output_10",
                                     "qualityMetrics"), temp = TRUE,
-                           tibble = FALSE, progress = TRUE,
-                           conflicts = "overwrite", recurse = FALSE,
-                           verbose = FALSE, ...) {
-        # osf id (for osfr), column types (for readr), and skip lines
+                            return = c("data.frame", "data.table", "tibble"),
+                            progress = TRUE, conflicts = "overwrite",
+                            recurse = FALSE, verbose = FALSE, ...) {
+        stopifnot(is.character(data))
+
+        # osf id (for osfr), column classes, and skip lines
         rinfo <- switch(data[1],
-                        inputDB        = list("9dsfk", "cccccciccdc", 1),
-                        output_5       = list("7tnfh", "ccccciiddd", 3),
-                        output_10      = list("43ucn", "ccccciiddd", 3),
-                        qualityMetrics = list("qpfw5", "ccDcdddcciiiiiildd", 1))
+                        inputDB        = list("9dsfk",
+                                              c("character","character",
+                                                "character","character",
+                                                "character","character",
+                                                "integer",  "character",
+                                                "character","character",
+                                                "numeric",  "character"),
+                                              1),
+                        Output_5       = list("7tnfh",
+                                              c("character","character",
+                                                "character","character",
+                                                "character","integer",
+                                                "integer",  "numeric",
+                                                "numeric",  "numeric"),
+                                              3),
+                        Output_10      = list("43ucn",
+                                              c("character","character",
+                                                "character","character",
+                                                "character","integer",
+                                                "integer",  "numeric",
+                                                "numeric",  "numeric"),
+                                              3),
+                        qualityMetrics = list("qpfw5",
+                                              c("character","character",
+                                                "Date",     "character",
+                                                "numeric",  "numeric",
+                                                "numeric",  "character",
+                                                "character","integer",
+                                                "integer",  "integer",
+                                                "integer",  "integer",
+                                                "integer",  "logical",
+                                                "numeric",  "numeric"),
+                                              1))
+        stopifnot(!is.null(rinfo)) # means 'data' wasn't one of the listed datasets
+
+        stopifnot(is.logical(temp))
         if (temp) {
                 path <- tempdir() # cleaned up after reading the dataset
         } else {
                 path <- getwd()
         }
+
         osf <- osfr::osf_retrieve_file(rinfo[[1]])
         osfr::osf_download(osf, path, progress = progress,
                            conflicts = conflicts, recurse = recurse,
                            verbose = verbose)
 
-        filepath <- file.path(path, paste0(data[1], ".zip"))
+        filename    <- paste0(data[1], ".zip")
+        zippath     <- file.path(path, filename)
+        archivefile <- file.path("Data", paste0(data[1], ".csv"))
 
-        out <- readr::read_csv(filepath, col_types = rinfo[[2]],
-                               skip = rinfo[[3]], ...)
-        if (temp) {
-                unlink(filepath) # temporary file cleanup
-                if (tibble) return (out)
-                return (as.data.frame(out))
-        } else {
-                if (tibble) return (out)
-                return (as.data.frame(out))
-        }
+        stopifnot(file.exists(zippath)) # The file was not downloaded or deleted
+        if (temp) on.exit(unlink(zippath), add = TRUE) # Cleanup
+
+        filepath <- unzip(zippath, archivefile, exdir = tempdir())
+        on.exit(unlink(filepath), add = TRUE) # Cleanup
+
+        message("Reading ", filepath)
+        out <- data.table::fread(filepath, sep = ',',
+                                 colClasses = rinfo[[2]], skip = rinfo[[3]], ...)
+
+        return <- return[1]
+        switch(return,
+               data.frame = return (collapse::qDF(out)),
+               data.table = return (out),
+               tibble     = return (collapse::qTBL(out)))
+
+        warning("Invalid return type specified, returning data.frame")
+        return (collapse::qDF(out))
 }
--- R/subset_covid.R
+++ R/subset_covid.R
@@ -1,5 +1,49 @@
+#' Subset any of the COVerAGE-DB datasets that is already read into memory.
+#'
+#' This function assumes the 'df' argument is one of the COVerAGE-DB datasets,
+#' or at least has a similar format. There's no check to test if this input is
+#' correct. Missing arguments except 'df' are ignored.
+#' The 'Date' argument can be either
+#' a character vector of a date, which will be converted to a 'Date' object,
+#' or alternatively a 'Date' object, which conversion will be skipped for.
+#' Dates are subsetted so the included dates are from the date inputted to the
+#' most recent date in the dataset. If multiple dates are passed, only the
+#' earliest date is taken into account.
+#' Countries that do not have regional data only have "All" set as their region.
+#' Subsetting is very fast due to the usage of the \pkg{collapse} package
+#' as a backend.
+#' @title Subset COVerAGE-DB Datasets
+#' @param df `data.frame`, `data.table`, or `tbl_df`.
+#' Expecting a COVerAGE-DB dataset.
+#' @param Country Character vector of countries to select.
+#' @param Region Character vector of regions to select.
+#' @param Sex Character vector of sexes to select. Usually either 'b' for both.
+#' 'f' for females, and 'm' for males.
+#' @param Date Either a character or Date vector of dates to include. If a
+#' character vector, then the date must be in "%Y-%m-%d" format.
+#' @return The subsetted data frame like object.
+#' @author Erez Shomron
+#' @examples
+#' \dontrun{
+#' # Nothing happens
+#' subset_covid(df)
+#' # Select several countries
+#' subset_covid(df, Country = c("USA", "Sweden"))
+#' # Sweden, females only
+#' subset_covid(df, Country = "Sweden", Sex = "f")
+#' # New York City
+#' subset_covid(df, Country = "USA", Region = "New York City")
+#' subset_covid(df, Region = "New York City")
+#' # All countries since "2020-12-01"
+#' subset_covid(df, Date = as.Date("2020-12-01"))
+#' subset_covid(df, Date = "2020-12-01")
+#' }
+#'
+#' @export
 subset_covid <- function(df, Country, Region, Sex, Date) {
         stopifnot(!missing(df))
+
+        date_is_date <- inherits(df$Date, "Date")
 
         if (!missing(Country)) {
                 c <- Country
@@ -15,8 +59,16 @@ subset_covid <- function(df, Country, Region, Sex, Date) {
         }
         if (!missing(Date)) {
                 stopifnot(inherits(Date, "Date"))
-                d <- Date
+                if (!date_is_date) {
+                        df <- collapse::ftransform(df, Date = as.Date(Date, "%d.%m.%Y"))
+                }
+                d  <- min(Date)
                 df <- collapse::fsubset(df, Date >= d)
+        }
+
+        # if df$Date wasn't a Date object, then convert back.
+        if (!date_is_date) {
+                df <- collapse::ftransform(df, Date = format(Date, "%d.%m.%Y"))
         }
 
         return (df)
